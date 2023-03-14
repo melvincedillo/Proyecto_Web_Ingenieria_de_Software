204f305d46ef857d81d6a842f4b7fcbe32613ba4
cedilloER <elias100cedillo@gmail.com>
2023-03-14 00:31:51 -0600

    Footer semi terminado y boostrap actualizado a version 5.2

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -19,7 +19,7 @@ namespace Proyecto_Web_Ingenieria_de_Software
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
