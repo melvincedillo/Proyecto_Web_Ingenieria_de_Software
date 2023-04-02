53d07f63ea7f0f296ba13fb169ed09da6b47344e
Rosángelica Castillo <rosangelicc97@gmail.com>
2023-04-02 17:54:27 -0600

    Cambios Rosa Front-Back end login Conexión BD

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LoginEntity.Startup))]
+namespace LoginEntity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
