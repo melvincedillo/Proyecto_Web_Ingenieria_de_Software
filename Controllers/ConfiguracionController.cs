49efc815226868a89e8d88b4bbefdfbbf1039415
cedilloER <elias100cedillo@gmail.com>
2023-04-11 23:59:21 -0600

    frontend usuarios

--- Controllers/ConfiguracionController.cs
+++ Controllers/ConfiguracionController.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Proyecto_Web_Ingenieria_de_Software.Models;
+using Proyecto_Web_Ingenieria_de_Software.Models.ViewModels;
 
 namespace Proyecto_Web_Ingenieria_de_Software.Controllers
 {
@@ -14,7 +16,21 @@ namespace Proyecto_Web_Ingenieria_de_Software.Controllers
         [PermisosModulos(moduloId:8)]
         public ActionResult Usuarios()
         {
-            return View();
+            List<UserViewModel> UserList = null;
+
+            using (BeautySalonEntities db = new BeautySalonEntities())
+            {
+                UserList = (from d in db.Users
+                       select new UserViewModel
+                       {
+                           ID = d.ID,
+                           dateCreate = d.UserCreateDate,
+                           userName = d.UserName,
+                           userActive = d.UserActive
+                       }).ToList();
+            }
+
+            return View(UserList);
         }
 
         // GET: Horarios
