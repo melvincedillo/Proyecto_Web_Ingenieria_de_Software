32f7465b8d84c7fbb1997d6b5f1d34e0acde602b
cedilloER <elias100cedillo@gmail.com>
2023-04-14 22:54:00 -0600

    Holiday

--- Controllers/HorarioController.cs
+++ Controllers/HorarioController.cs
@@ -1,4 +1,6 @@
 ﻿using Proyecto_Web_Ingenieria_de_Software.Filters;
+using Proyecto_Web_Ingenieria_de_Software.Models;
+using Proyecto_Web_Ingenieria_de_Software.Models.AddModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,9 +14,67 @@ namespace Proyecto_Web_Ingenieria_de_Software.Controllers
     {
         // GET: Horario
         [PermisosModulos(moduloId: 6)]
+        [HttpGet]
         public ActionResult Index()
         {
+            List<Horario> listHorario = null;
+            List<Holiday> listHoliday = null;
+
+            using (var db = new BeautySalonEntities())
+            {
+                listHorario = (from d in db.Horario
+                               where d.Weekday == true
+                               select d).ToList();
+
+                listHoliday = (from d in db.Holiday
+                              where d.Date >= DateTime.Today
+                              select d).ToList();
+            }
+
+            ViewBag.Horarios = listHorario;
+            ViewBag.Holidays = listHoliday;
             return View();
+        }
+
+        // POST: Horarios
+        [PermisosModulos(moduloId: 6)]
+        [HttpPost]
+        public ActionResult AddHoliday(DateTime Date, string motivo)
+        {
+            try
+            {
+                if(Date != null && motivo != null)
+                {
+                    using (var db = new BeautySalonEntities())
+                    {
+                        var holiday = new Holiday();
+                        holiday.Date = Date;
+                        holiday.Description = motivo;
+                        db.Holiday.Add(holiday);
+                        db.SaveChanges();
+                    }
+                }
+                return RedirectToAction("Index", "Horario");
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Index", "Horario");
+            }
+            
+        }
+
+        [PermisosModulos(moduloId: 6)]
+        [HttpPost]
+        public ActionResult DeleteHoliday(int id)
+        {
+            using (var db = new BeautySalonEntities())
+            {
+                Holiday holiday = db.Holiday.Find(id);
+                db.Holiday.Remove(holiday);
+                db.SaveChanges();
+            }
+
+            return Content("1");
         }
     }
 }
