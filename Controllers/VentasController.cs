f8646283d6b510f578d5fbf6c3ab3131108a529d
Rosangélica Castillo <rosangelicac97@gmail.com>
2023-04-16 14:05:27 -0600

    mapear a la bd la factura y el detalle

--- Controllers/VentasController.cs
+++ Controllers/VentasController.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Proyecto_Web_Ingenieria_de_Software.Models.ViewModels;
-
+using Proyecto_Web_Ingenieria_de_Software.Models;
 
 namespace Proyecto_Web_Ingenieria_de_Software.Controllers
 {
@@ -46,9 +46,52 @@ namespace Proyecto_Web_Ingenieria_de_Software.Controllers
             return View();
         }
 
-        public ActionResult Agregar()
+        public ActionResult Agregar(FacturaViewModel factura, List <FacturaDetalleViewModel> detalle)
         {
+            double subTotal=0;
+            foreach(var i in detalle)
+            {
+                subTotal = i.Price + subTotal;
+            }
+            using (var db = new BeautySalonEntities())
+            {
+                Factura oFactura = new Factura();
+                
 
+                oFactura.Time = factura.Time;
+                oFactura.Total = (decimal)subTotal;
+                oFactura.Tax = (decimal)factura.Tax;
+                oFactura.ClientName = factura.ClientName;
+                oFactura.SalonID = factura.SalonID;
+                oFactura.EmployeeID = factura.EmployeeID;
+
+                var facturita = db.Factura.Add(oFactura);
+                db.SaveChanges();
+
+                int idFactura = db.Factura.Max(x => x.FacturaNumero);
+
+                //detallefactura
+                FacturaDetalle oDetalle = null;
+                foreach (var i in detalle)
+                {
+                   oDetalle= new FacturaDetalle();
+
+                    oDetalle.FacturaNumero = idFactura;
+                    oDetalle.SalonID = oDetalle.SalonID;
+                    oDetalle.ProductID = oDetalle.ProductID;
+                    oDetalle.ServiceID = oDetalle.ServiceID;
+                    oDetalle.TaxID = oDetalle.TaxID;
+                    oDetalle.Price = oDetalle.Price;
+                    oDetalle.Quantity = oDetalle.Quantity;
+                    oDetalle.Discount = oDetalle.Discount;
+                    oDetalle.SalesTax = oDetalle.SalesTax;
+
+                    var detalleFact = db.FacturaDetalle.Add(oDetalle);
+                    db.SaveChanges();
+
+
+                }
+            }
         }
 
 
